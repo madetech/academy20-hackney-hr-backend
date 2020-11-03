445e65f64edb0741022a09134e81402077a05b72
Chloe <chloe.wong@madetech.com>
2020-11-03 14:43:37 +0000

    created HomeController.cs

---
+++ api/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("")]
+    public class HomeController : ControllerBase
+    {
+        private readonly ILogger<HomeController> _logger;
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+        [HttpGet]
+        public object Get()
+        {
+            var responseObject = new
+            {
+                Status = "Up"
+            };
+            _logger.LogInformation($"Status pinged: {responseObject.Status}");
+            return responseObject;
+        }
+    }
+}
