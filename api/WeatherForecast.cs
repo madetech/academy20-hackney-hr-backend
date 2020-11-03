278a01c68ef614962350467fed531b9c374522e9
Ben <ben.dalton@madetech.com>
2020-11-03 12:07:38 +0000

    added API.sln file, added WebAPI Template, API xunit test

--- api/WeatherForecast.cs
+++ api/WeatherForecast.cs
@@ -1,6 +1,6 @@
 using System;
 
-namespace api
+namespace API
 {
     public class WeatherForecast
     {
--- tests/unit/api_endpoint_tests.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace unit
-{
-    public class Class1
-    {
-    }
-}
