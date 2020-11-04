70ddaccc88591e9d491468cb8a0748427e88ec38
Ben <ben.dalton@madetech.com>
2020-11-04 10:29:12 +0000

    create user login controller, user details & login models

--- api/Controllers/UserLoginController.cs
+++ api/Controllers/UserLoginController.cs
@@ -1,7 +1,19 @@
-namespace API.Controllers
-{
-    public class UserLoginController
-    {
-        
-    }
-}
+using System;
+using API.Models;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+// namespace API.Controllers
+// {
+//     public class UserLoginController
+//     {
+//         [Route("api/[controller]")]
+//         [ApiController]
+
+//         public class UserLoginController : ControllerBase
+//         {
+
+//         }
+//     }
+// }
