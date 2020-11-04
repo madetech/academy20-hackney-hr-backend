a5bce968b49ebf85444e5350617c40ed6dd1ed59
Chloe <chloe.wong@madetech.com>
2020-11-04 17:52:08 +0000

    modified UserDetails.cs to include all the attributes in the mock json file

--- api/Models/UserDetails.cs
+++ api/Models/UserDetails.cs
@@ -4,18 +4,17 @@ namespace API.Models
 {
     public class UserDetails
     {
-        public string name { get; set; }
+        public long id { get; set; }
+        public string first_name { get; set; }
+        public string last_name { get; set; }
         public string job_title { get; set; }
-        public string email { get; set; }
-
+        public string contact_email { get; set; }
         public decimal salary { get; set; }
-
-        public string office { get; set; }
-
-        public string contact_information { get; set; }
-
-        public DateTime date_of_birth { get; set; }
-        public string next_of_kin { get; set; }
+        public string street_address { get; set; }
+        public string postcode { get; set; }
+        public string next_of_kin_first_name { get; set; }
+        public string next_of_kin_last_name { get; set; }
 
     }
 }
+
