79b8a8fa322b6fddbe24c8796b0c404df24ff119
Chloe <chloe.wong@madetech.com>
2020-11-05 21:31:44 +0000

    renamed userdetails.cs to user.cd

---
+++ api/Models/User.cs
@@ -0,0 +1,18 @@
+namespace API.Models
+{
+    public class Employee
+    {
+        public long id { get; set; }
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+        public string job_title { get; set; }
+        public string contact_email { get; set; }
+        // public decimal salary { get; set; }
+        // public string street_address { get; set; }
+        // public string postcode { get; set; }
+        // public string next_of_kin_first_name { get; set; }
+        // public string next_of_kin_last_name { get; set; }
+
+    }
+}
+
