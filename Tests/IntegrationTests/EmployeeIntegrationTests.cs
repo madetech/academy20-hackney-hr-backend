a14f7a02cd295ca627382cb0fe17706731b05b8d
Chloe <chloe.wong@madetech.com>
2020-11-09 15:59:36 +0000

    pass api get request test

--- Tests/IntegrationTests/EmployeeIntegrationTests.cs
+++ Tests/IntegrationTests/EmployeeIntegrationTests.cs
@@ -1,18 +1,48 @@
-using System;
-using System.Runtime.ExceptionServices;
-using Xunit;
-using System.Text;
-using Api.Controllers;
-using System.Threading.Tasks;
+using System.Collections.Generic;
 using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+using Api.Controllers;
+using Api.Models;
+using System;
+
 
 namespace Api.Tests.IntegrationTests 
 {
     public class EmployeeIntegrationTests
-    : IClassFixture<WebApplicationFactory<Api.Startup>>
+        : IClassFixture<WebApplicationFactory<Api.Startup>>
     {
-    [Fact]
-    public async Task GetEmployeeList()
+        private readonly HttpClient httpClient;
+
+        public EmployeeIntegrationTests(WebApplicationFactory<Api.Startup> factory)
+        {
+            httpClient = factory.CreateClient();
+        }
+
+        [Fact]
+        public async Task GetEmployeeList()
+        {
+            // arrange
+            
+            // act
+            var response = await httpClient.GetAsync("api/employee");
+            // assert
+            var stringResponse = await response.Content.ReadAsStringAsync();
+            var terms = JsonSerializer.Deserialize<List<Employee>>(stringResponse, new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase });
+
+            Assert.Equal(4, terms.Count);
+            Assert.Contains(terms, t => t.id == 1);
+            Assert.Contains(terms, t => t.first_name == "Ben");
+            Assert.Contains(terms, t => t.last_name == "Dalton");
+            Assert.Contains(terms, t => t.job_title == "Academy Software Engineer");
+            Assert.Contains(terms, t => t.contact_email == "ben.dalton@madetech.com");
+
+        }
     }
+    
 } 
