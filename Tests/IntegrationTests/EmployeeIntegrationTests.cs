69dc23640fd56664be92303a329be2fe79def6a6
Chloe <chloe.wong@madetech.com>
2020-11-09 12:27:53 +0000

    changed casing of folder and file api

--- Tests/IntegrationTests/EmployeeIntegrationTests.cs
+++ Tests/IntegrationTests/EmployeeIntegrationTests.cs
@@ -6,3 +6,13 @@ using Api.Controllers;
 using System.Threading.Tasks;
 using System.Net;
 using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace Api.Tests.IntegrationTests 
+{
+    public class EmployeeIntegrationTests
+    : IClassFixture<WebApplicationFactory<Api.Startup>>
+    {
+    [Fact]
+    public async Task GetEmployeeList()
+    }
+}
