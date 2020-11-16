ecc887ca9337dd4cfc60169fd0e7d0834d7f5351
Derek Baker <derek.baker89@gmail.com>
2020-11-16 14:06:24 +0000

    updated models to match database data fields

--- Api/Models/User.cs
+++ Api/Models/User.cs
@@ -2,12 +2,24 @@ namespace Api.Models
 {
     public class Employee
     {
-        public long id { get; set; }
+        public int id { get; set; }
         public string first_name { get; set; }
         public string last_name { get; set; }
         public string job_title { get; set; }
         public string contact_email { get; set; }
-
+        public long contact_number { get; set; }
+        public string salary_band { get; set; }
+        public string home_address_line_1 { get; set; }
+        public string home_address_line_2 { get; set; }
+        public string home_address_city {get; set; }
+        public string home_address_postcode { get; set; }
+        public string office_location {get; set; }
+        public string manager { get; set; }
+        public string reportees { get; set; }
+        public string next_of_kin_first_name { get; set; }
+        public string next_of_kin_last_name { get; set; }
+        public long next_of_kin_contact_number { get; set; }
+        
     }
 }
 
--- Api/Models/UserLogin.cs
+++ Api/Models/UserLogin.cs
@@ -4,7 +4,13 @@ namespace Api.Models
 {
     public class UserLogin
     {
-        public string Username { get; set; }
-        public string Password { get; set; }
+        public int employee_id { get; set; }
+        public string contact_email { get; set; }
+        public string password { get; set; }
+        public bool manager { get; set; }
+        public bool admin { get; set; }
+        public bool employee { get; set; }
+        public bool contractor_or_volunteer { get; set; }
+
     }
 }
